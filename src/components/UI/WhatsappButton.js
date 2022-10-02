3f7c6e1519f68d1706583f7f5aefdccc10918f70
Rodrigo <rodridega@gmail.com>
2022-10-02 15:59:10 -0300

    add alt on img tags

--- src/components/UI/WhatsappButton.js
+++ src/components/UI/WhatsappButton.js
@@ -4,7 +4,7 @@ import wp from "../../assets/Vector.svg"
 
 export const WhatsappButton = () => {
     return (
-        <a href={"https://wa.me/573246147929"} target="_blank" type='button' className='btn btn-vivvi w-100 w-md-50 mt-3 text-focus-in' ><img src={wp} className="mx-1" />
+        <a href={"https://wa.me/573246147929"} target="_blank" rel='noreferrer' type='button' className='btn btn-vivvi w-100 w-md-50 mt-3 text-focus-in' ><img src={wp} className="mx-1" alt='whatsapp-icon' />
             ¡QUIERO REMODELAR!</a>
     )
 }
